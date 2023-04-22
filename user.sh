5630bb056f7f866526959839a847cb8f867e9bfa
darkroom <arafique458@gmail.com>
2023-04-22 17:31:30 -0400

    Correction systemctl restart user

--- user.sh
+++ user.sh
@@ -37,7 +37,7 @@ echo -e "\e[36m>>>>>>>>>> Installing MongoDB Client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
 echo -e "\e[36m>>>>>>>>>> Restarting MongoDB Client <<<<<<<<<<\e[0m"
-systemctl restart catalogue
+systemctl restart user
 
 echo -e "\e[36m>>>>>>>>>> Loading Schema Final <<<<<<<<<<\e[0m"
 mongo --host mongodb.devopsdude.cloud </app/schema/user.js

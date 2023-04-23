33978e54d978243f754829a3ff9415fa000535ac
darkroom <arafique458@gmail.com>
2023-04-23 10:13:23 -0400

    Configure MySQL with the echo statments

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,16 @@
+echo -e "\e[36m>>>>>>>>>> Disabling MYSQL 8 Version <<<<<<<<<<\e[0m"
 dnf module disable mysql -y
+
+echo -e "\e[36m>>>>>>>>>> Copying MySQL Repo File <<<<<<<<<<\e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>> Install MySQL <<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>>> Starting MYSQL <<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[36m>>>>>>>>>> Reseting MYSQL Password <<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1

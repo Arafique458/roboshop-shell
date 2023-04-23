997711c58d753ed42454fd9d2577b079bfc32e6b
darkroom <arafique458@gmail.com>
2023-04-23 10:26:37 -0400

    Configure Shipping with the echo statments

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,31 @@
+echo -e "\e[36m>>>>>>>>>> Installing Maven <<<<<<<<<<\e[0m"
 yum install maven -y
-cp shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>>>> Setting up Systemd Service <<<<<<<<<<\e[0m"
+cp shipping.service /root/roboshop-shell/etc/systemd/system/shipping.service
+
+echo -e "\e[36m>>>>>>>>>> Adding user <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>> Creating Application Directory <<<<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>> Extract App Content <<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>>> Downloading Maven Dependencies <<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[36m>>>>>>>>>> Installing MYSQL <<<<<<<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>>> Loading Schema <<<<<<<<<<\e[0m"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>>> Restarting Shipping <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-yum install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping

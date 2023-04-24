5c165e6bf623aa7af955792008136ad31c5107da
darkroom <arafique458@gmail.com>
2023-04-23 22:39:23 -0400

    Configuring Payment.sh and adding echo statements.

--- payment.sh
+++ payment.sh
@@ -1,14 +1,28 @@
+echo -e "\e[36m>>>>>>>>>> Installing Python <<<<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
+
+echo -e "\e[36m>>>>>>>>>> Adding Application user <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>> Creating Application Directory <<<<<<<<<<\e[0m"
+rmdir -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>> Downloading Payment Zip <<<<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> Extracting the files <<<<<<<<<<\e[0m"
 unzip /tmp/payment.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>> Installing Python Dependencies <<<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
-cp payment.service /etc/systemd/system/payment.service
+echo -e "\e[36m>>>>>>>>>> Setting up Systemd Service <<<<<<<<<<\e[0m"
+cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
 
+echo -e "\e[36m>>>>>>>>>> Starting the Payment Service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment

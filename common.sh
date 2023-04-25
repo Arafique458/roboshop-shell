dcf8338226768d0518074ad83991830fc7ca4b79
darkroom <arafique458@gmail.com>
2023-04-24 21:16:54 -0400

    Declaring fucntion in common.sh
    added print_head for the echo statement

--- common.sh
+++ common.sh
@@ -1,34 +1,43 @@
 app_user=roboshop
 
+# function for echo statements
+print_head(){
+  echo -e "\e[35m>>>>>>>>>> $* <<<<<<<<<<\e[0m"
+
+}
+
 #cart function
 func_nodejs(){
+
+# If we want the input to be considered on we will use double quote "" and change the print head value from $* to $1
+  print_head "Configuring NodeJS Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  echo -e "\e[36m>>>>>>>>>> Installing NodeJS <<<<<<<<<<\e[0m"
+  print_head "Installing NodeJS"
   yum install nodejs -y
 
-  echo -e "\e[36m>>>>>>>>>> Application user Added <<<<<<<<<<\e[0m"
+  print_head "Application user Added"
   useradd ${app_user}
 
-  echo -e "\e[36m>>>>>>>>>> Creating Application Directory <<<<<<<<<<\e[0m"
+  print_head "Creating Application Directory"
   rm -rf /app
   mkdir /app
 
-  echo -e "\e[36m>>>>>>>>>> Downloading App Content <<<<<<<<<<\e[0m"
+  print_head "Downloading App Content"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
-  echo -e "\e[36m>>>>>>>>>> Unzipping App Content <<<<<<<<<<\e[0m"
+   print_head "Unzipping App Content"
   unzip /tmp/${component}.zip
   cd /app
 
-  echo -e "\e[36m>>>>>>>>>> Installing NodeJS Dependencies <<<<<<<<<<\e[0m"
+   print_head "Installing NodeJS Dependencies"
   npm install
 
-  echo -e "\e[36m>>>>>>>>>> Coping service <<<<<<<<<<\e[0m"
+   print_head "Copying Service"
   cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[36m>>>>>>>>>> Starting cart Service <<<<<<<<<<\e[0m"
+   print_head "Starting cart Service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl start ${component}

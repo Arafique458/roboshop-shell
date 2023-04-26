d4812dccb8c3bb39cb5794abf08c9f2b9a6dff75
darkroom <arafique458@gmail.com>
2023-04-25 21:15:22 -0400

    Changes made to shipping.sh Making code DRY

--- common.sh
+++ common.sh
@@ -1,57 +1,89 @@
 app_user=roboshop
 
 # function for echo statements
-print_head(){
+func_print_head(){
   echo -e "\e[35m>>>>>>>>>> $* <<<<<<<<<<\e[0m"
 
 }
 
-schema_setup(){
-  echo -e "\e[36m>>>>>>>>>> copying mongoDB repo <<<<<<<<<<\e[0m"
+func_schema_setup(){
+
+if [ "$schema_setup" == "mongo"]; then
+  func_print_head "copying mongoDB repo"
   cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 
-  echo -e "\e[36m>>>>>>>>>> Installing MongoDB Client <<<<<<<<<<\e[0m"
+  func_print_head "Installing MongoDB Client"
   yum install mongodb-org-shell -y
 
-  echo -e "\e[36m>>>>>>>>>> Loading Schema Final <<<<<<<<<<\e[0m"
+  func_print_head "Loading Schema Final "
   mongo --host mongodb.devopsdude.cloud </app/schema/${component}.js
+fi
+if [ "$schema_setup" == "mysql"]; then
+
+  func_print_head "Installing MYSQL"
+  yum install mysql -y
+
+  func_print_head "Loading Schema "
+  mysql -h mysql.devopsdude.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+fi
+}
+
+
+func_app_prereq(){
+  func_print_head "Create Application User"
+    useradd ${app_user}
+
+    func_print_head "Creating Application Directory"
+    rm -rf /app
+    mkdir /app
+
+    func_print_head "Downloading Application Content"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
+
+     func_print_head "Unzipping Application Content"
+    unzip /tmp/${component}.zip
+}
+
+func_systemd_setup(){
+  func_print_head "Copying Service"
+    cp $script_path/${component}.service /etc/systemd/system/${component}.service
+
+     func_print_head "Starting cart Service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl start ${component}
 }
 
 #cart function
 func_nodejs(){
 
 # If we want the input to be considered on we will use double quote "" and change the print head value from $* to $1
-  print_head "Configuring NodeJS Repos"
+  func_print_head "Configuring NodeJS Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head "Installing NodeJS"
+  func_print_head "Installing NodeJS"
   yum install nodejs -y
 
-  print_head "Application user Added"
-  useradd ${app_user}
+  func_app_prereq
 
-  print_head "Creating Application Directory"
-  rm -rf /app
-  mkdir /app
-
-  print_head "Downloading App Content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-   print_head "Unzipping App Content"
-  unzip /tmp/${component}.zip
-  cd /app
-
-   print_head "Installing NodeJS Dependencies"
+  func_print_head "Installing NodeJS Dependencies"
   npm install
 
-   print_head "Copying Service"
-  cp $script_path/${component}.service /etc/systemd/system/${component}.service
-
-   print_head "Starting cart Service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl start ${component}
-
-  schema_setup
+  func_systemd_setup
+  func_schema_setup
 }
+
+func_java(){
+  func_print_head "Installing Maven"
+  yum install maven -y
+
+  func_app_prereq
+
+  func_print_head "Downloading Maven Dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+  func_schema_setup
+  func_systemd_setup
+  }
--- shipping.sh
+++ shipping.sh
@@ -7,35 +7,7 @@ if [ -z "$mysql_root_password"]; then
   echo MYSQL root Password is Missing
 fi
 
-echo -e "\e[36m>>>>>>>>>> Installing Maven <<<<<<<<<<\e[0m"
-yum install maven -y
+component="shipping"
+schema_setup=mysql
 
-echo -e "\e[36m>>>>>>>>>> Adding user <<<<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>>> Creating Application Directory <<<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>>> Extract App Content <<<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>>>>>> Downloading Maven Dependencies <<<<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>>>>> Installing MYSQL <<<<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>>> Loading Schema <<<<<<<<<<\e[0m"
-mysql -h mysql.devopsdude.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>>>>>> Setting up Systemd Service <<<<<<<<<<\e[0m"
-cp $script_path/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>>> Restarting Shipping <<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+func_java

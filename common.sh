92bb761c87f39a98f495eae116399be932c58786
Arafique458 <arafique458@gmail.com>
2023-04-26 13:40:25 -0400

    Log_file added

--- common.sh
+++ common.sh
@@ -1,3 +1,6 @@
+script=$(realpath "0")
+script_path=$(dirname "$script")
+log_file=/tmp/roboshop.log
 app_user=roboshop
 
 # function for echo statements
@@ -19,64 +22,64 @@ func_schema_setup(){
 
 if [ "$schema_setup" == "mongo" ]; then
   func_print_head "copying mongoDB repo"
-  cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
+  cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
   func_print_head "Installing MongoDB Client"
-  yum install mongodb-org-shell -y
+  yum install mongodb-org-shell -y &>>$log_file
 
   func_print_head "Loading Schema Final "
-  mongo --host mongodb.devopsdude.cloud </app/schema/${component}.js
+  mongo --host mongodb.devopsdude.cloud </app/schema/${component}.js &>>$log_file
 fi
 if [ "$schema_setup" == "mysql" ]; then
 
   func_print_head "Installing MYSQL"
-  yum install mysql -y
+  yum install mysql -y &>>$log_file
 
   func_print_head "Loading Schema "
-  mysql -h mysql.devopsdude.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+  mysql -h mysql.devopsdude.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
 fi
 }
 
 
 func_app_prereq(){
   func_print_head "Create Application User"
-    useradd ${app_user}
+    useradd ${app_user} &>>$log_file
 
     func_print_head "Creating Application Directory"
     rm -rf /app
-    mkdir /app
+    mkdir /app &>>$log_file
 
     func_print_head "Downloading Application Content"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
     cd /app
 
      func_print_head "Unzipping Application Content"
-    unzip /tmp/${component}.zip
+    unzip /tmp/${component}.zip &>>$log_file
 }
 
 func_systemd_setup(){
   func_print_head "Copying Service"
-    cp $script_path/${component}.service /etc/systemd/system/${component}.service
+    cp $script_path/${component}.service /etc/systemd/system/${component}.service &>>$log_file
 
      func_print_head "Starting ${component} Service"
-    systemctl daemon-reload
-    systemctl enable ${component}
-    systemctl start ${component}
+    systemctl daemon-reload &>>$log_file
+    systemctl enable ${component} &>>$log_file
+    systemctl start ${component} &>>$log_file
 }
 
 #cart function
 func_nodejs(){
 # If we want the input to be considered on we will use double quote "" and change the print head value from $* to $1
   func_print_head "Configuring NodeJS Repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
   func_print_head "Installing NodeJS"
-  yum install nodejs -y
+  yum install nodejs -y &>>$log_file
 
   func_app_prereq
 
   func_print_head "Installing NodeJS Dependencies"
-  npm install
+  npm install &>>$log_file
 
   func_schema_setup
   func_systemd_setup
@@ -85,18 +88,18 @@ func_nodejs(){
 
 func_java(){
   func_print_head "Installing Maven"
-  yum install maven -y
+  yum install maven -y &>>$log_file
 
   func_stat_check $? # ($#) Argument is being sent through here.
 
   func_app_prereq
 
   func_print_head "Downloading Maven Dependencies"
-  mvn clean package
+  mvn clean package &>>$log_file
 
   func_stat_check $?
 
-  mv target/${component}-1.0.jar ${component}.jar
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
 
   func_schema_setup
   func_systemd_setup
